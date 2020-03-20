84c85d172d45674d20bc0debb2aa1a88d3532245
Jack Nelson <Jack.Nelson245@gmail.com>
2020-03-20 11:21:35 -0700

    Update __init__.py
    adding . prefixes

--- salabim_plus/__init__.py
+++ salabim_plus/__init__.py
@@ -1,6 +1,6 @@
 from .salabim_plus import Environment, EntityGenerator, EntityTracker, Entity, Machine, MachineGroup, ShiftController, Worker, Kanban, Storage
-import misc_tools
-import output_viewer
+import .misc_tools
+import .output_viewer
 
 __all__ = [Entity,
            EntityGenerator,
