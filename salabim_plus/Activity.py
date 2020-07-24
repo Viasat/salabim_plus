890f11db90eb4d4ad10a8a3e62cac85316149f48
JackNelson <jack.nelson245@gmail.com>
2020-07-24 00:01:36 -0700

    rev1 of Activity animation

--- salabim_plus/Activity.py
+++ salabim_plus/Activity.py
@@ -11,30 +11,40 @@ class Activity(sim.Component):
     def setup(
         self,
         process_func,
-        # time,
         process_cap=1,
         in_buffer_cap=np.inf,
         out_buffer_cap=np.inf,
         in_unbatch=False,
         in_batch_qty=1,
         out_unbatch=True,
-        out_batch_qty=None
+        out_batch_qty=None,
+        animate=False,
+        x=None,
+        y=None,
+        w=None,
+        h=None,
+        fillcolor="blue",
+        textcolor="white"
     ):
 
         # assigning input parameters    
         self.process_func = process_func
-        # self.time = time
         self.process_cap = process_cap
         self.in_unbatch = in_unbatch
         self.in_batch_qty = in_batch_qty
         self.out_unbatch = out_unbatch
         self.out_batch_qty = out_batch_qty
+        self.animate = animate
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.fillcolor = fillcolor
+        self.textcolor = textcolor
         
         # creating activity buffers 
         self._in_buffer = simx.Buffer(activity=self, buffer_type='in', cap=in_buffer_cap)
-        # self._in_buffer.activate()
         self._to_process = simx.Buffer(activity=self, buffer_type='to_process')
-#         self._start_staging = Buffer(activity=self, buffer_type='start_staging', cap=1)
         self._processing = simx.Buffer(activity=self, buffer_type='processing', cap=process_cap)
         self._complete = simx.Buffer(activity=self, buffer_type='complete')
         self._out_buffer = simx.Buffer(activity=self, buffer_type='out', cap=out_buffer_cap)
@@ -43,7 +53,6 @@ class Activity(sim.Component):
         self._in_gate = simx.Gate(activity=self, gate_type='in')
         self._process_gate = simx.Gate(activity=self, gate_type='process')
         self._out_gate = simx.Gate(activity=self, gate_type='out')
-#         self._start_process = sim.State(name=self.name()+'_start_process')
 
         # creating processors and unutilized processor holding area
         self._processor_q = sim.Queue(name=self.name()+'_processor_q')
@@ -54,6 +63,9 @@ class Activity(sim.Component):
         self._processor_assigned = sim.State(name=self.name()+'_processor_assigned')
         self._processee_q = sim.Queue(name=self.name()+'_processee_q')
 
+        # creating animation if initiated
+        self._make_animation()
+
     def process(self):
 
         while True:
@@ -62,3 +74,20 @@ class Activity(sim.Component):
             processor.processee = self._processee_q.pop()
             self._processor_assigned.trigger(max=1)
             processor.activate(process="activity")
+
+    def _make_animation(self):
+
+        if self.animate:
+            print(f'test {self.name()}')
+            sim.AnimateRectangle(
+                spec=(0, 0, self.w, self.h),
+                x=self.x,
+                y=self.y,
+                xy_anchor="nw",
+                fillcolor=self.fillcolor,
+                text=self.name(),
+                textcolor=self.textcolor,
+                text_anchor="c",
+                arg = self
+                # parent=self,
+            )
--- salabim_plus/Buffer.py
+++ salabim_plus/Buffer.py
@@ -19,15 +19,17 @@ class Buffer(sim.Component):
         self._is_full = sim.State(name=self.name()+'_is_full', value='NO')
         self._txn = sim.State(name=self.name()+'_txn')
         self._txn_done = sim.State(name=self.name()+'_txn_done')
+
+        self._make_animation()
     
     def process(self):
         
         while True:
-#             yield self.wait((self._lvl, self.txn))
             yield self.wait(self._txn)
             self._lvl.set(len(self._q))
             self.check_is_full()
             self._txn_done.trigger(max=1)
+            self._make_animation()
     
     def is_full(self):
         return self._is_full.value()
@@ -41,6 +43,62 @@ class Buffer(sim.Component):
     def has_one(self, x):
         v, c, s = x
         return v > 0
+
+    def _make_animation(self):
+
+        if self._activity.animate:
+            print(f'buffer test {self._activity.name()} {self.buffer_type}')
+
+            x, y, text_anchor = self._get_an_xy()
+
+            if self.is_full() == 'YES':
+                textcolor = "red"
+            else:
+                textcolor = self._activity.textcolor
+
+            sim.AnimateText(
+                text=str(self.lvl()),
+                x = x,
+                y = y,
+                text_anchor = text_anchor,
+                textcolor = textcolor,
+                arg = self
+                # parent = self
+            )
+
+    def _get_an_xy(self):
+
+        if self.buffer_type == 'in':
+            x = self._activity.x + self._activity.w*0.1
+            y = self._activity.y - self._activity.h*0.1
+            text_anchor = "nw"
+
+        elif self.buffer_type == 'to_process':
+            x = self._activity.x + self._activity.w*0.1
+            y = self._activity.y + self._activity.h - self._activity.h*0.1
+            text_anchor = "sw"
+
+        elif self.buffer_type == 'processing':
+            x = self._activity.x + self._activity.w*0.5
+            y = self._activity.y + self._activity.h*0.5
+            text_anchor = "c"
+
+        elif self.buffer_type == 'complete':
+            x = self._activity.x + self._activity.w - self._activity.w*0.1
+            y = self._activity.y + self._activity.h - self._activity.h*0.1
+            text_anchor = "se"
+
+        elif self.buffer_type == 'out':
+            x = self._activity.x + self._activity.w - self._activity.w*0.1
+            y = self._activity.y - self._activity.h*0.1
+            text_anchor = "ne"
+
+        else:
+            x = self._activity.x
+            y = self._activity.y
+            text_anchor = "e"
+
+        return (x, y, text_anchor)
     
     def check_is_full(self):
         
